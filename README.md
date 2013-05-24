98c08350038aeee38452fb4f495fa4de2ce11d20
stottp <stotty@server.fake>
2013-05-24 15:21:02 +0100

    Remove some target files

---
+++ README.md
@@ -0,0 +1,7 @@
+This github respository contains sample code in various languages to help customers get started with writing betfair API-NG clients.   
+
+Each sample demonstrates a basic use case of placing a bet on the next GB Horse Racing market.
+
+A comprehensive description of the samples can be found at TODO
+
+The sample code is not intended to be used as-is in a production environment and carries no assurances or guarantees.
---
+++ php/jsonrpc.php
@@ -0,0 +1,210 @@
+<?php
+
+if (count($argv) != 3) {
+    echo 'usage: php -f jsonrpc.php AppKey SessionToken';
+    exit(-1);
+}
+
+
+$APP_KEY = $argv[1];
+$SESSION_TOKEN = $argv[2];
+
+// Setting DEBUG to true will output all request / responses to api-ng.
+$DEBUG = False;
+
+echo "1. Get all Event Types....\n";
+$allEventTypes = getAllEventTypes($APP_KEY, $SESSION_TOKEN);
+
+echo "\n2. Extract Event Type Id for Horse Racing....\n";
+$horseRacingEventTypeId = extractHorseRacingEventTypeId($allEventTypes);
+
+echo "\n3. EventTypeId for Horse Racing is: $horseRacingEventTypeId \n";
+
+echo "\n4. Get next horse racing market in the UK....\n";
+$nextHorseRacingMarket = getNextUkHorseRacingMarket($APP_KEY, $SESSION_TOKEN, $horseRacingEventTypeId);
+
+echo "\n5. Print static marketId, name and runners....\n";
+printMarketIdAndRunners($nextHorseRacingMarket);
+
+echo "\n6. Get volatile info for Market including best 3 exchange prices available...\n";
+$marketBook = getMarketBook($APP_KEY, $SESSION_TOKEN, $nextHorseRacingMarket->marketId);
+
+echo "\n7. Print volatile price data along with static runner info....\n";
+printMarketIdRunnersAndPrices($nextHorseRacingMarket, $marketBook);
+
+echo "\n\n8. Place a bet below minimum stake to prevent the bet actually being placed....\n";
+$betResult = placeBet($APP_KEY, $SESSION_TOKEN, $nextHorseRacingMarket->marketId, $nextHorseRacingMarket->runners[0]->selectionId);
+
+echo "\n9. Print result of bet....\n\n";
+printBetResult($betResult);
+
+
+function getAllEventTypes($appKey, $sessionToken)
+{
+
+    $jsonResponse = sportsApingRequest($appKey, $sessionToken, 'listEventTypes', '{"filter":{}}');
+
+    return $jsonResponse[0]->result;
+
+}
+
+function extractHorseRacingEventTypeId($allEventTypes)
+{
+    foreach ($allEventTypes as $eventType) {
+        if ($eventType->eventType->name == 'Horse Racing') {
+            return $eventType->eventType->id;
+        }
+    }
+
+}
+
+function getNextUkHorseRacingMarket($appKey, $sessionToken, $horseRacingEventTypeId)
+{
+
+    $params = '{"filter":{"eventTypeIds":["' . $horseRacingEventTypeId . '"],
+              "marketCountries":["GB"],
+              "marketTypeCodes":["WIN"],
+              "marketStartTime":{"from":"' . date('c') . '"}},
+              "sort":"FIRST_TO_START",
+              "maxResults":"1",
+              "marketProjection":["RUNNER_DESCRIPTION"]}';
+
+    $jsonResponse = sportsApingRequest($appKey, $sessionToken, 'listMarketCatalogue', $params);
+
+    return $jsonResponse[0]->result[0];
+}
+
+function printMarketIdAndRunners($nextHorseRacingMarket)
+{
+
+    echo "MarketId: " . $nextHorseRacingMarket->marketId . "\n";
+    echo "MarketName: " . $nextHorseRacingMarket->marketName . "\n\n";
+
+    foreach ($nextHorseRacingMarket->runners as $runner) {
+        echo "SelectionId: " . $runner->selectionId . " RunnerName: " . $runner->runnerName . "\n";
+    }
+
+}
+
+
+function getMarketBook($appKey, $sessionToken, $marketId)
+{
+    $params = '{"marketIds":["' . $marketId . '"], "priceProjection":{"priceData":["EX_BEST_OFFERS"]}}';
+
+    $jsonResponse = sportsApingRequest($appKey, $sessionToken, 'listMarketBook', $params);
+
+    return $jsonResponse[0]->result[0];
+}
+
+
+function printMarketIdRunnersAndPrices($nextHorseRacingMarket, $marketBook)
+{
+
+    function printAvailablePrices($selectionId, $marketBook)
+    {
+
+        // Get selection
+        foreach ($marketBook->runners as $runner)
+            if ($runner->selectionId == $selectionId) break;
+
+        echo "\nAvailable to Back: \n";
+        foreach ($runner->ex->availableToBack as $availableToBack)
+            echo $availableToBack->size . "@" . $availableToBack->price . " | ";
+
+        echo "\n\nAvailable to Lay: \n";
+        foreach ($runner->ex->availableToLay as $availableToLay)
+            echo $availableToLay->size . "@" . $availableToLay->price . " | ";
+
+    }
+
+
+    echo "MarketId: " . $nextHorseRacingMarket->marketId . "\n";
+    echo "MarketName: " . $nextHorseRacingMarket->marketName;
+
+    foreach ($nextHorseRacingMarket->runners as $runner) {
+        echo "\n\n\n===============================================================================\n";
+
+        echo "SelectionId: " . $runner->selectionId . " RunnerName: " . $runner->runnerName . "\n";
+        echo printAvailablePrices($runner->selectionId, $marketBook);
+    }
+}
+
+function placeBet($appKey, $sessionToken, $marketId, $selectionId)
+{
+
+    $params = '{"marketId":"' . $marketId . '",
+                "instructions":
+                     [{"selectionId":"' . $selectionId . '",
+                       "handicap":"0",
+                       "side":"BACK",
+                       "orderType":
+                       "LIMIT",
+                       "limitOrder":{"size":"1",
+                                    "price":"1000",
+                                    "persistenceType":"LAPSE"}
+                       }], "customerRef":"fsdf"}';
+
+    $jsonResponse = sportsApingRequest($appKey, $sessionToken, 'placeOrders', $params);
+
+    return $jsonResponse[0]->result;
+
+}
+
+function printBetResult($betResult)
+{
+    echo "Status: " . $betResult->status;
+
+    if ($betResult->status == 'FAILURE') {
+        echo "\nErrorCode: " . $betResult->errorCode;
+        echo "\n\nInstruction Status: " . $betResult->instructionReports[0]->status;
+        echo "\nInstruction ErrorCode: " . $betResult->instructionReports[0]->errorCode;
+    } else
+        echo "Warning!!! Bet placement succeeded !!!";
+}
+
+
+function sportsApingRequest($appKey, $sessionToken, $operation, $params)
+{
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, "https://beta-api.betfair.com/json-rpc");
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        'X-Application: ' . $appKey,
+        'X-Authentication: ' . $sessionToken,
+        'Accept: application/json',
+        'Content-Type: application/json'
+    ));
+
+    $postData =
+        '[{ "jsonrpc": "2.0", "method": "SportsAPING/v1.0/' . $operation . '", "params" :' . $params . ', "id": 1}]';
+
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
+
+    debug('Post Data: ' . $postData);
+    $response = json_decode(curl_exec($ch));
+    debug('Response: ' . json_encode($response));
+
+    curl_close($ch);
+
+    if (isset($response[0]->error)) {
+        echo 'Call to api-ng failed: ' . "\n";
+        echo  'Response: ' . json_encode($response);
+        exit(-1);
+    } else {
+        return $response;
+    }
+
+}
+
+function debug($debugString)
+{
+    global $DEBUG;
+    if ($DEBUG)
+        echo $debugString . "\n\n";
+}
+
+?>
+
+
+
