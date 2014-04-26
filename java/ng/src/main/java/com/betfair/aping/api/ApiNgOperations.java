d724039c7dcc78a8186caf893b2aea0aeae1efcc
Denes Kelenyi <denes.kelenyi@me.com>
2014-04-25 18:36:53 -0600

    Fixing missing marketProjection filter when calling listMarketCatalogue method

--- java/ng/src/main/java/com/betfair/aping/api/ApiNgOperations.java
+++ java/ng/src/main/java/com/betfair/aping/api/ApiNgOperations.java
@@ -13,9 +13,9 @@ import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
 
-
 public abstract class ApiNgOperations {
-	protected final String FILTER = "filter";
+
+    protected final String FILTER = "filter";
     protected final String LOCALE = "locale";
     protected final String SORT = "sort";
     protected final String MAX_RESULT = "maxResults";
@@ -24,18 +24,18 @@ public abstract class ApiNgOperations {
     protected final String INSTRUCTIONS = "instructions";
     protected final String CUSTOMER_REF = "customerRef";
     protected final String locale = Locale.getDefault().toString();
+    protected final String MARKET_PROJECTION = "marketProjection";
 
-	public abstract List<EventTypeResult> listEventTypes(MarketFilter filter, String appKey, String ssoId) throws APINGException;
+    public abstract List<EventTypeResult> listEventTypes(MarketFilter filter, String appKey, String ssoId) throws APINGException;
 
-	public abstract List<MarketBook> listMarketBook(List<String> marketIds, PriceProjection priceProjection, OrderProjection orderProjection,
-						MatchProjection matchProjection, String currencyCode, String appKey, String ssoId) throws APINGException;
+    public abstract List<MarketBook> listMarketBook(List<String> marketIds, PriceProjection priceProjection, OrderProjection orderProjection,
+            MatchProjection matchProjection, String currencyCode, String appKey, String ssoId) throws APINGException;
 
     public abstract List<MarketCatalogue> listMarketCatalogue(MarketFilter filter, Set<MarketProjection> marketProjection,
-        MarketSort sort, String maxResult, String appKey, String ssoId) throws APINGException;
+            MarketSort sort, String maxResult, String appKey, String ssoId) throws APINGException;
 
-	public abstract PlaceExecutionReport placeOrders(String marketId, List<PlaceInstruction> instructions, String customerRef , String appKey, String ssoId) throws APINGException;
+    public abstract PlaceExecutionReport placeOrders(String marketId, List<PlaceInstruction> instructions, String customerRef, String appKey, String ssoId) throws APINGException;
 
-    protected abstract String makeRequest(String operation, Map<String, Object> params, String appKey, String ssoToken) throws  APINGException;
+    protected abstract String makeRequest(String operation, Map<String, Object> params, String appKey, String ssoToken) throws APINGException;
 
 }
-
